ebd2d158daede774fe5d45c54e9f13b24ce2a997
zxcasd89525 <zxcasd89525@gmail.com>
2021-10-21 02:15:50 +0800

    update

--- pic_detect.js
+++ pic_detect.js
@@ -3,13 +3,20 @@ let ctx = canvas1.getContext("2d");
 let sendbtn = document.getElementById('sendbtn');
 let img_origin = document.getElementById('img')
 let file_upload = document.getElementById('file')
-const jsonUpload = document.getElementById('json-upload');
-const weightsUpload = document.getElementById('weights-upload');
 let upload = document.getElementById('upload');
-let img = new Image;
+let img;
+let label = ["David" , "Wayne"];
 sendbtn.addEventListener("click",function(){
+    //let imagedata = ctx.getImageData(0,0,canvas1.width,canvas1.height);
     loadmodel().then(model =>{
-        model.summary()
+        const raw = tf.browser.fromPixels(canvas1,3);
+        let resized = tf.image.resizeBilinear(raw, [model.inputs[0].shape[1],model.inputs[0].shape[2]]);
+	    let tensor = resized.expandDims(0);
+        let prediction = model.predict(tensor);
+        let pIndex = tf.argMax(prediction, 1).dataSync();
+        model.predict(tensor).print();
+        //console.log(prediction);
+        //model.summary()
     })
 })
 async function loadmodel(){
@@ -18,6 +25,7 @@ async function loadmodel(){
 }
 upload.addEventListener('click',function(){
     loadmodel().then(model =>{
-        console.log(model.inputs[0].shape[1]);
+        //console.log(model.inputs[0].shape[1]);
+        console.log(model.summary());
     })
 })
